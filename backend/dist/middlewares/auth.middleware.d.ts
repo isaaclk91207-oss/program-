d4169226dcedc9a764f7537c0000a90f40a0b422
Isaac LK <isaaclk91207@gmail.com>
2026-09-08 22:31:33 +0630

    chore: untrack node_modules, dist, and secrets for clean builds

--- backend/dist/middlewares/auth.middleware.d.ts
+++
@@ -1,5 +0,0 @@
-import { Response, NextFunction } from "express";
-import { AuthenticatedRequest, JwtPayload } from "../types";
-export declare function authenticate(req: AuthenticatedRequest, _res: Response, next: NextFunction): void;
-export declare function generateToken(payload: JwtPayload): string;
-//# sourceMappingURL=auth.middleware.d.ts.map
--- backend/dist/middlewares/rbac.middleware.d.ts
+++
@@ -1,4 +0,0 @@
-import { Response, NextFunction } from "express";
-import { AuthenticatedRequest } from "../types";
-export declare function requireRole(...allowedRoles: string[]): (req: AuthenticatedRequest, _res: Response, next: NextFunction) => void;
-//# sourceMappingURL=rbac.middleware.d.ts.map
